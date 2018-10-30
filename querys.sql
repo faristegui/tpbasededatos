6584e01c3b5c7a8e99663c869718846f0824e580
Matias Lahore <matias.lahore@ayi-asociados.com>
2018-10-30 10:48:52 -0300

    Query N°1

--- querys.sql
+++ querys.sql
@@ -1,11 +1,14 @@
 /* 1- Consultar los turnos disponibles de X médico con Y obra social (del cliente) y con una desde-hasta fecha.*/
 
-SELECT CONCAT(m.nombre, ' ' , m.apellido) as medico, am
-FROM medico m, agenda_medico am
+SELECT CONCAT(m.nombre, ' ' , m.apellido) as medico, am.fecha as dia, CONCAT(am.horaDesde, '-' , am.horaHasta) as hora_turno
+FROM medico m, agenda_medico am, realiza_prestacion rp, prestacion p
 WHERE m.nombre = 'Matias'
-AND m.apellido = 'Almeyda';
-
-
+AND m.apellido = 'Almeyda'
+AND m.nroDoc = am.nroDoc
+AND am.fecha > '2018-02-04'
+AND am.fecha < '2018-03-10'
+AND rp.idPrestacion = p.idPrestacion
+AND p.nombre = 'Protesis';
 
 
 /* 2- Traer los últimos 10 turnos realizados por un medico con su paciente y la obra social que atendió. */
