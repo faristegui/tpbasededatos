3ec7ea08472a0daa1feed113a39213bb59fabac8
FAristegui <faristegui@fi.uba.ar>
2018-11-20 20:00:46 -0300

    Cambios en la consulta 2

--- querys.sql
+++ querys.sql
@@ -17,10 +17,7 @@ SELECT CONCAT(m.nombre, ' ' , m.apellido) as medico,
 	CONCAT(p.nombre, ' ' , p.apellido) as paciente , pl.nombre as plan
 FROM medico m, atencion_turno atur, turno t, paciente p, plan pl
 WHERE m.nroDoc = atur.nroDoc
-AND (SELECT m.nroDoc
-	FROM medico m
-	WHERE m.nombre = 'Veronica'
-	AND m.apellido = 'Duran') = m.nroDoc
+AND  m.nombre = 'Veronica' AND m.apellido = 'Duran'
 AND atur.idTurno = t.idTurno
 AND t.nroDoc = p.nroDoc
 AND t.idPlan = pl.idPlan
